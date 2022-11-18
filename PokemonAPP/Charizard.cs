cadfd46a795afa880e84772fb4b23d6529a27bf2
mac <wardust007@gmail.com>
2022-11-17 16:27:45 -0800

    "making cool pokemon app witht he group!"

---
+++ PokemonAPP/Charizard.cs
@@ -0,0 +1,18 @@
+namespace pokemonApp;
+
+class Charizard  : Charmeleon 
+{
+    int powerLevel = 1000000;
+    String name = "Charizard";
+    String color = "Orange & Blue";
+
+    String mainPower = " Solar Power";
+
+
+
+    public static void charizard()
+    {
+        System.Console.WriteLine("Solar Pooooooooooooooooooooooooooooooooowers!");
+    }
+
+}
---
+++ PokemonAPP/Charmeleon.cs
@@ -0,0 +1,16 @@
+namespace pokemonApp;
+
+class Charmeleon : Charmander
+{   int pweoweLevel = 190;
+    String color = "RED";
+    String name = "Charmeleon";
+    
+public static void charmeleon()
+{
+   
+        System.Console.WriteLine("sharp claws");
+}
+
+         
+     
+}
